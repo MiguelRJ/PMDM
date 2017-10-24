6a1f857695e544aec3d49bce04bc210083433030
MiguelRJ <miguel.rj96@gmail.com>
2017-10-24 14:38:51 +0200

    fin de lase, sprites eventos boton, canvas

---
+++ Clase1020Canvas/Assets/scripts/saludame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saludame : MonoBehaviour {
+
+	public void Saluda(){
+		Debug.Log ("Hola soy el evento del boton");
+	}
+}
