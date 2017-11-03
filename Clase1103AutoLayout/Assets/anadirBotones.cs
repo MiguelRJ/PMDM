150f25ca7ce1a757d704aae7fa4c6467899f63bb
MiguelRJ <miguel.rj96@gmail.com>
2017-11-03 10:15:15 +0100

    Scroll

---
+++ Clase1103AutoLayout/Assets/anadirBotones.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anadirBotones : MonoBehaviour {
+
+	GameObject _primerBoton;		// Boton a clonar
+	GameObject _padreDeLosBotones; 	// Contenido
+
+
+	// Use this for initialization
+	void Start () {
+		_primerBoton = GameObject.Find ("btnPrimero");
+		_padreDeLosBotones = GameObject.Find ("Contenido");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AnadirBoton() {
+		// Anado un clon del boton "btnPrimero" como hijo del GameObject "Contenido"
+		Instantiate(_primerBoton,_padreDeLosBotones.GetComponent<Transform>());
+	}
+}
