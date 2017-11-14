25089dfca821a94b4a16d8f3c35879361205ee09
MiguelRJ <miguel.rj96@gmail.com>
2017-11-14 13:42:48 +0100

    Fisicas

---
+++ Clase1110InmediateGUI/Assets/scripts/Interfaz.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interfaz : MonoBehaviour {
+	
+	string _texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n Aliquam in pellentesque ex. Fusce sodales, arcu nec malesuada imperdiet, felis diam tempus sapien, id porta ex elit quis justo. \n In maximus erat a tortor dapibus, sed laoreet risus mollis. \n Aliquam ullamcorper auctor velit sed lobortis. Praesent non odio at lorem iaculis auctor eu sed massa. \n Suspendisse vel velit mattis, vehicula tortor at, efficitur arcu. \n Integer ultricies sem lorem, et venenatis nunc imperdiet quis.";
+	public float _velocidadScroll = 2F;
+	TextMesh _componente3dText;
+	Vector3 pos;
+	Vector3 localVectorUp;
+
+	// Use this for initialization
+	void Start () {
+		_componente3dText = gameObject.GetComponent<TextMesh> ();
+		//_componente3dText.text = _texto;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = transform.position;
+		localVectorUp = transform.TransformDirection (0, 1, 0);
+		pos += localVectorUp * _velocidadScroll * Time.deltaTime;
+		transform.position = pos;
+	}
+
+	// Inmediate Mode GUI (IMGUI)
+	//void OnGUI(){
+		//if (GUILayout.Button ("Pulsame")) {
+		//	Debug.Log ("Hola caracola");
+		//}
+	//}
+
+}
---
+++ Clase1114Fisicas/Assets/script/Fisica.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fisica : MonoBehaviour {
+
+	public float _velocidad = 50F; // Fuerza de empuea en funion de la amsa y el rozamiento
+	public Rigidbody _rigidbody;
+
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+
+	void FixedUpdate () {
+		Vector3 fuerzaEmpuje = new Vector3 (
+			Input.GetAxis("Horizontal") * _velocidad,
+			0,
+			Input.GetAxis("Vertical") * _velocidad);
+		_rigidbody.AddForce (fuerzaEmpuje);
+	}
+}
