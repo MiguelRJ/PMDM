497ccc8d3bad7f615c990250a70f2074d1a9bf9f
MiguelRJ <miguel.rj96@gmail.com>
2017-11-17 10:09:04 +0100

    Fisicas meteorito

---
+++ Clase1117FisicaColliderMeteorito/Assets/Movimineto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimineto : MonoBehaviour {
+
+	public float _velocidad = 1F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
