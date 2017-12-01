bdc35b39202c5c3e45d3a68d0feb771d749684a0
MiguelRJ <miguel.rj96@gmail.com>
2017-12-01 10:16:13 +0100

    sphere cast

---
+++ Clase1128Rayos/Assets/DetectarClickEnObjeto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectarClickEnObjeto : MonoBehaviour {
+
+	Ray _pulsacion;
+	RaycastHit _colision;// Almacena informacion o datos como querais
+
+	void Start () {
+			
+	}
+
+	void Update () {
+		if (Input.GetMouseButton (0)) { // 0, boton izquiero del raton
+			_pulsacion = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast (_pulsacion, out _colision)) {
+				Debug.Log (_colision.collider.name);
+			}
+		}
+	}
+}
---
+++ Clase1201SphereCast/Assets/EsferaCast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaCast : MonoBehaviour {
+
+	Vector3 posicionInicial;
+	float radio;
+	Vector3 direccionRayoEsfera;
+	RaycastHit infoImpacto;
+	float distancia;
+
+	void Start () {
+		radio = transform.localScale.x / 2;
+		direccionRayoEsfera = Vector3.right;
+		distancia = 5;
+	}
+
+	void Update () {
+		// personaje = gameObject.GetComponent<CharacterController> ();
+		posicionInicial = transform.position;
+		if (Physics.SphereCast(posicionInicial,radio,direccionRayoEsfera,out infoImpacto,distancia,1,QueryTriggerInteraction.UseGlobal)){
+			Debug.Log ("Detectado obstaculo a "+infoImpacto.distance);
+		}
+		Debug.DrawRay (transform.position, direccionRayoEsfera*5, Color.green);
+	}
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(posicionInicial+direccionRayoEsfera*distancia, radio);
+	}
+
+}
