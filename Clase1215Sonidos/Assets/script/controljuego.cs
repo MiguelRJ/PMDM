5b80d220122a3a158e9020c6e5320f6b0384839b
MiguelRJ <miguel.rj96@gmail.com>
2017-12-15 09:27:32 +0100

    Clase sonidos

---
+++ Clase1215Sonidos/Assets/script/controljuego.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controljuego : MonoBehaviour {
+
+	public AudioSource sonidoDestruir;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision colision){
+		if (colision.gameObject.tag == "Enemigo") {
+			Debug.Log ("Ha colisionado con..." + colision.gameObject.name);
+			//colision.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+			sonidoDestruir.Play();
+			if (colision.relativeVelocity.magnitude > 10F) {
+				Destroy (colision.gameObject);
+			}
+		}
+	}
+
+	void OnCollisionStay(Collision colision){
+		if (colision.gameObject.tag == "Enemigo") {
+			Debug.Log ("Ha colisionado con..." + colision.gameObject.name);
+			colision.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		}
+	}
+
+	void OnCollisionExit(Collision colision){
+		if (colision.gameObject.tag == "Enemigo") {
+			Debug.Log ("Ha dejado de colisionar con..." + colision.gameObject.name);
+			colision.gameObject.GetComponent<Renderer> ().material.color = Color.gray;
+		}
+	}
+
+
+}
